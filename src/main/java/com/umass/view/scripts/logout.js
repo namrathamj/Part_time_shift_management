ba36b695a39fbb1a8594e3f6e184321bf271b69e
Richa-123-singh <richasingh234@gmail.com>
2023-12-08 14:14:02 -0500

    added logout js

---
+++ src/main/java/com/umass/view/scripts/logout.js
@@ -0,0 +1,8 @@
+// Assuming you have a function to handle the logout process
+function handleLogout() {
+    // Perform logout actions here (e.g., clear session, redirect, etc.)
+    alert("You have logged out successfully!");
+}
+
+// Attach the function to the button click event
+document.getElementById("logoutButton").addEventListener("click", handleLogout);
